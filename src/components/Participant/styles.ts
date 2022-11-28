1aafd523085e06c43ae2404e19bc2bad8b8ac879
Christian Viana <viana2015christian@gmail.com>
2022-11-28 00:06:40 -0300

    Criado tela home com componente de participante

---
+++ src/components/Participant/styles.ts
@@ -0,0 +1,35 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        backgroundColor: '#1F1E25',
+        borderRadius: 5,
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 10
+    },
+
+    name: {
+        flex: 1,
+        fontSize: 16,
+        marginLeft: 16,
+        color: '#FFFFFF',
+
+    },
+
+    buttonText: {
+        color: '#ffffff',
+        fontSize: 24,
+    },
+
+    button: {
+        backgroundColor: '#E23C44',
+        height: 56,
+        width: 56,
+        borderRadius: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+
+    },
+})
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,37 @@
+import { Text, View, TextInput, TouchableOpacity } from 'react-native'
+import { Participant } from '../../components/Participant'
+import { styles } from './styles'
+
+export function Home() {
+
+    function handleParticipantAdd() {
+        console.log('Voce clicou para adicionar')
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.eventName}>
+                Nome de Evento
+            </Text>
+            <Text style={styles.eventDate}>
+                Sexta, 4 de Novembro de 2022
+            </Text>
+            <View style={styles.form}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Nome do participante"
+                    placeholderTextColor="#6b6b6b"
+                />
+
+                <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+                    <Text style={styles.buttonText}>
+                        +
+                    </Text>
+                </TouchableOpacity>
+            </View>
+            <Participant />
+            <Participant />
+            <Participant />
+        </View>
+    )
+}
